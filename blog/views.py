21421f111962a79992b51e1e963bcb51f2459fd8
diskrace <diskrace@nate.com>
2019-02-13 11:28:50 +0900

    year커스텀컨버터

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def articles_by_year(request, year):
+    return HttpResponse(f'''
+    {year}년도에 대한 목록
+    ''')
 def index(request):
     return render(request, 'blog/index.html')
 
-    
+
 def hello_times(request, times):
     message = "안녕하세요" * times
     return HttpResponse(message)
