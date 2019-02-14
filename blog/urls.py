43b7fb604a28e4c12aa51f83db15881d46291a02
diskrace <diskrace@nate.com>
2019-02-14 11:05:49 +0900

    네이버블로그검색 추가

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from blog.views import articles_by_year
 from django.urls import register_converter
 from blog.converters import FourDigitYearConverter
 from blog.converters import SlugUnicodeConverter
-from blog.views import naver_realtime_keywords
+from blog.views import naver_realtime_keywords 
+from blog.views import naver_blog_search
 
 register_converter(FourDigitYearConverter, 'year')
 register_converter(SlugUnicodeConverter, 'slug_unicode')
@@ -16,5 +17,6 @@ urlpatterns = [
     path('articles/<year:year>/', articles_by_year),
     # re_path(r'blog/hello_times/(?P<times>\d+)/$, hello_times),  
     path('naver/실시간검색어/', naver_realtime_keywords),
+    path('naver/네이버블로그검색/', naver_blog_search),
     path('', index), 
 ]
